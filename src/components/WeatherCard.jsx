aad4332e172ec5402ead63b63481d7560a8c1d4c
Rajasabari N <rajasabari296@gmail.com>
2026-02-25 11:33:16 +0530

    Update WeatherCard.jsx

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -2,17 +2,10 @@ function WeatherCard({ weather }) {
   return (
     <div className="card">
       <h2>{weather.name}</h2>
-
-      <img
-        src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-        alt="icon"
-      />
-
       <p>Temperature: {weather.main.temp} °C</p>
       <p>Humidity: {weather.main.humidity}%</p>
       <p>Weather: {weather.weather[0].description}</p>
     </div>
   );
 }
-
 export default WeatherCard;
