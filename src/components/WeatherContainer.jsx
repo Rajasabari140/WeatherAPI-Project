0889c457c38e6028a125bd832747c9586af80bd0
Rajasabari N <rajasabari296@gmail.com>
2026-02-25 11:35:09 +0530

    Update WeatherContainer.jsx

--- src/components/WeatherContainer.jsx
+++ src/components/WeatherContainer.jsx
@@ -4,7 +4,6 @@ import WeatherCard from "./WeatherCard";
 import { fetchWeather } from "../services/weatherService";
 
 function WeatherContainer() {
-
   const [weather, setWeather] = useState(null);
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
@@ -13,10 +12,8 @@ function WeatherContainer() {
     try {
       setLoading(true);
       setError("");
-
       const data = await fetchWeather(city);
       setWeather(data);
-
     } catch (err) {
       setError(err.message);
       setWeather(null);
@@ -24,16 +21,13 @@ function WeatherContainer() {
       setLoading(false);
     }
   };
-
   return (
     <>
       <Searchbar onSearch={handleSearch} />
-
       {loading && <p>Loading...</p>}
       {error && <p className="error">{error}</p>}
       {weather && <WeatherCard weather={weather} />}
     </>
   );
 }
-
 export default WeatherContainer;
