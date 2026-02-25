20dd10f7910210d564530c57b8995c6577e4c376
Rajasabari N <rajasabari296@gmail.com>
2026-02-25 11:31:18 +0530

    Update Searchbar.jsx

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,26 +1,15 @@
 import { useState } from "react";
 
 function SearchBar({ onSearch }) {
-
   const [city, setCity] = useState("");
-
   const handleSearch = () => {
     if (city.trim() !== "") {
-      onSearch(city);
-    }
-  };
-
+      onSearch(city);}};
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Enter city"
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
-      />
+      <input type="text" placeholder="Enter city" value={city}onChange={(e) => setCity(e.target.value)}/>
       <button onClick={handleSearch}>Search</button>
     </div>
   );
 }
-
 export default SearchBar;
